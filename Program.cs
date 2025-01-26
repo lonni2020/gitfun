7a2b37f1aeab85850091fa748acb954594894998
lonni2020 <153124510+lonni2020@users.noreply.github.com>
2025-01-25 20:49:54 -0700

    slay

--- Program.cs
+++ Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-Console.WriteLine("This is a line of code");
-
-Console.WriteLine("This is eventually going to be a mistake");
